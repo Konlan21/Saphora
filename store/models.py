173487ed7f1915f9e85c7a80b0f4527b759a08b6
Konlan21 <joekonlan32@gmail.com>
2023-03-08 18:44:20 +0000

    Build app data structure

--- store/models.py
+++ store/models.py
@@ -1,3 +1,97 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT)
+    first_name = models.CharField(max_length=255, null=True)
+    last_name = models.CharField(max_length=255, null=True)
+    email = models.EmailField(unique=True)
+
+    def __str__(self) -> str:
+        full_name = f'{self.first_name, self.last_name}'
+        return full_name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField(null=True)
+    digital = models.BooleanField(default=False, null=True, blank=False)
+
+    # image = models.ImageField(null=True, blank=True)  
+    def __str__(self):
+        return self.name
+	# # Below property tells, tht if no image is uploaded,
+	# # it will show broken image picture
+    # @property
+	# def imageURL(self):
+	# 	try:
+	# 		url = self.image.url
+	# 	except:
+	# 		url = ''
+	# 	return url
+
+
+class Order(models.Model):
+    PAYMENT_COMPLETED = 'C'
+    PAYMENT_PENDING = 'P'
+    PAYMENT_FAILED = 'F'
+    PAYMENT_STATUS = [
+        (PAYMENT_COMPLETED, 'Completed'),
+        (PAYMENT_PENDING, 'Pending'),
+        (PAYMENT_FAILED, 'Failed')
+    ]
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=150, null=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS, default=PAYMENT_PENDING)
+    def __str__(self):
+        return str(self.id)
+
+    # @property
+    # def shipping(self):
+    #     shipping = False
+    #     orderitems = self.orderitem_set.all()
+    #     for i in orderitems:
+    #         # if, a product is not digital, then it has to be shipped
+    #         if i.product.digital == False:
+    #             shipping = True
+    #     return shipping
+    
+    # @property
+    # def get_cart_total(self):
+    #     orderitems = self.orderitem_set.all()
+    #     total = sum([item.get_total for item in orderitems])
+    #     return total
+    
+    # @property
+    # def get_cart_items(self):
+    #     orderitems = self.orderitem_set.all()
+    #     total = sum([item.quantity for item in orderitems])
+    #     return total
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True )
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    # @property
+    # def get_total(self):
+    #     total = self.product.price * self.quantity
+    #     return total
+
+
+    
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
+    address  = models.CharField(max_length=250, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self) -> str:
+        return self.address

edbee375e9cbbf37e6ef14824a4937a5e5595a72
Konlan21 <joekonlan32@gmail.com>
2023-03-11 21:50:18 +0000

    Render dynamic data into template setup

--- saphora/urls.py
+++ saphora/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 admin.site.site_header = 'Saphora Admin'
 admin.site.index_title = 'Admin'
@@ -23,3 +25,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include("store.urls"))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/models.py
+++ store/models.py
@@ -3,35 +3,31 @@ from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
-    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.PROTECT)
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.PROTECT)
     first_name = models.CharField(max_length=255, null=True)
     last_name = models.CharField(max_length=255, null=True)
     email = models.EmailField(unique=True)
 
     def __str__(self) -> str:
-        full_name = f'{self.first_name, self.last_name}'
-        return full_name
+        return f'{self.last_name, self.first_name}'
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField(null=True)
     digital = models.BooleanField(default=False, null=True, blank=False)
-
-    # image = models.ImageField(null=True, blank=True)  
+    image = models.ImageField(null=True, blank=True)  
     def __str__(self):
         return self.name
-	# # Below property tells, tht if no image is uploaded,
-	# # it will show broken image picture
-    # @property
-	# def imageURL(self):
-	# 	try:
-	# 		url = self.image.url
-	# 	except:
-	# 		url = ''
-	# 	return url
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
-
+        
 class Order(models.Model):
     PAYMENT_COMPLETED = 'C'
     PAYMENT_PENDING = 'P'
@@ -59,17 +55,17 @@ class Order(models.Model):
     #             shipping = True
     #     return shipping
     
-    # @property
-    # def get_cart_total(self):
-    #     orderitems = self.orderitem_set.all()
-    #     total = sum([item.get_total for item in orderitems])
-    #     return total
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
     
-    # @property
-    # def get_cart_items(self):
-    #     orderitems = self.orderitem_set.all()
-    #     total = sum([item.quantity for item in orderitems])
-    #     return total
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
@@ -77,10 +73,10 @@ class OrderItem(models.Model):
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
-    # @property
-    # def get_total(self):
-    #     total = self.product.price * self.quantity
-    #     return total
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
     

42c0829bad85c8050c28c592bcf9923d74ef8a64
Konlan21 <joekonlan32@gmail.com>
2023-05-10 20:48:02 +0000

    Implement product-detail view with dynamic URL parameter

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import *
 from django.http import JsonResponse
 import json
@@ -19,6 +19,13 @@ def store(request):
     products = Product.objects.all()
     context = {'products': products, 'cartItems': cartItems}
     return render(request, 'store/store.html', context)
+
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    context = {'product': product}
+    return render(request, 'store/product_detail.html', context)
+
 
 def cart(request):
     if request.user.is_authenticated:

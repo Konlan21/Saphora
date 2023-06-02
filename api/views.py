e861db0ef7e52de4abc0baf63029f827edb18b08
Konlan21 <joekonlan32@gmail.com>
2023-06-02 23:36:51 +0000

    Create viewsets for API endpoints

--- api/views.py
+++ api/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from store.models import *
+from .serializers import *
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
-    serializer_class = 
+    serializer_class = ProductSerializer
+
+class CustomerViewSet(ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class CollectionViewSet(ModelViewSet):
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer

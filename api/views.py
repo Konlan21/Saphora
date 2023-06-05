0bfb52aae6201034c14042320da12117af4b4449
Konlan21 <joekonlan32@gmail.com>
2023-06-05 14:59:02 +0000

    Add sorting functionality to API endpoints

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import *
-from rest_framework.filters import SearchFilter
+from rest_framework.filters import SearchFilter, OrderingFilter  
 from django_filters.rest_framework import DjangoFilterBackend
 from store.models import *
 from .serializers import *
@@ -9,9 +9,10 @@ from .serializers import *
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer  
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['name']
     search_fields = ['name', 'description']
+    ordering_fields = ['price']
 
     def get_permissions(self):
         if self.request.method == 'GET':
@@ -22,18 +23,20 @@ class CustomerViewSet(ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]  
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['first_name']
     search_fields = ['first_name', 'last_name', 'email']
+    ordering_fields = ['first_name', 'last_name']
 
     
     
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.all()
     serializer_class = CollectionSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['title']
     search_fields = ['title']
+    ordering_fields = ['id']
 
     def get_permissions(self):
         if self.request.method == 'GET':
@@ -44,6 +47,7 @@ class CollectionViewSet(ModelViewSet):
 class OrderViewset(ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['id']
-    search_fields = ['id', 'date_ordered', 'payment_status', 'completed']
+    search_fields = ['id', 'date_ordered', 'payment_status']
+    ordering_fields = ['date_ordered', 'payment_status']

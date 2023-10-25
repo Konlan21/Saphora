acebea3488fe31d64ca9b70288678b474b76dd22
Konlan21 <joekonlan32@gmail.com>
2023-10-25 15:48:07 +0000

    Turn debug off

--- saphora/settings.py
+++ saphora/settings.py
@@ -18,7 +18,7 @@ import cloudinary_storage
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-DEBUG = True
+DEBUG = False
 
 SECRET_KEY = "django-insecure-7c7f9ke71q!xary4!m*u-r-xpkk#m!n0q4-seu=#k+!uyj@b$_"
 

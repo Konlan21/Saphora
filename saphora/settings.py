678aad169fb91ba7d3c7261fc6f7945495d9f194
Konlan21 <joekonlan32@gmail.com>
2023-10-25 15:23:10 +0000

    Ignore static and media dirs

--- saphora/settings.py
+++ saphora/settings.py
@@ -15,7 +15,7 @@ import os
 import cloudinary_storage
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 DEBUG = True
